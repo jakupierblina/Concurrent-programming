9bd553c06d344a2056f3782022526cc47b6367e3
Erblina Jakupi <higo5t@INF.ELTE.HU>
2020-12-21 02:44:39 +0100

    lab4

--- exercises/PingPong.java
+++ exercises/PingPong.java
@@ -1,44 +1,65 @@
-public class PingPong implements Runnable{
+/*public interface Runnable{
+
+		public void run();
+}*/
+
+
+
+public class PingPong implements Runnable
+{
 	
 
 	private String word;
 	private int delay;
 
-	public PingPon(String word, int delay){
+	public PingPong(String word, int delay)
+	{
 		this.word = word;
 		this.delay = delay;
 	}
 
 
 	//add a delay
-	public void run(){
-		try{
+	public void run()
+	{
+		try
+		{
 			//run this much time
-			for(int i=0; i<20; i++){
+			System.out.println(Thread.currentThread().getName());
+			for(int i=0; i<20; i++)
+			{
 				System.out.println(word);
 				Thread.sleep(delay); //static method 
 			}
-		}catch(InterruptedException ex){
+		}
+		catch(InterruptedException ex)
+		{
 			return;
 		}
 	}
 	
 
-	public static void main(String [] args){
+	public static void main(String [] args)
+	{
 		//polimorfizem n runable method
 
 		PingPong ping = new PingPong("Ping", 30);
 		Runnable pong = new PingPong("Pong", 30);
 
 
-		PingPong t1 = new Thread(ping);
-		t1.start();
+		//new PingPon ("Ping", 30). start();
+
+		/*PingPong ping = new PingPong("Ping", 30);
+		Runnable pong = new PingPong("Pong", 30);
+
+
+		Thread t1 = new Thread(ping);
+		t1.start();*/
+
+		new Thread(ping).start();
 
 		Thread t2 = new Thread(pong);
+		t2.setName("Thread2");
 		t2.start();
-
-
-		
-
 	}
 }
